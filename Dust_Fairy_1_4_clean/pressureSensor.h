ebb3ac0e61d8d4b1ca7709dd37883acc9065feff
Aleksey Khoroshilov <56565199+aleksey00@users.noreply.github.com>
2022-09-29 14:11:31 -0400

    pressureSensor.cpp .h comments

--- Dust_Fairy_1_4_clean/pressureSensor.h
+++ Dust_Fairy_1_4_clean/pressureSensor.h
@@ -10,7 +10,6 @@
 
 const byte pressureInput = A2;                      // analog input pin for the Solenoid base pressure transducer
 const byte pressureInputMain = A4;                  // analog input pin for the Main system pressure transducer
-// const int pressureZero = 102.4;                     // analog reading of pressure transducer at 0psi
 const int pressureZero = 98.6;                      // analog reading of pressure transducer at 0psi
 const int pressureMax = 921.6;                      // analog reading of pressure transducer at 100psi
 const int pressuretransducermaxPSI = 300;           // psi value of transducer being used
