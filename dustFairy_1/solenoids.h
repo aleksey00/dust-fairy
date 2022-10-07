d7215b8a83be04a4c689d519c551bd7f6f05856f
Aleksey Khoroshilov <56565199+aleksey00@users.noreply.github.com>
2022-10-06 23:13:03 -0400

    Delete dustFairy_1 directory

--- dustFairy_1/solenoids.h
+++
@@ -1,6 +0,0 @@
-//solenoids
-const int solenoid1 = 12;  //SawBlade Close
-const int solenoid2 = 11;  //SawBlade Open
-const int solenoid3 = 10;  //EdgeBander Close
-const int solenoid4 = 9;   //EdgeBander Open
-const int vacuum = 2; //Dust-collector solenoid
